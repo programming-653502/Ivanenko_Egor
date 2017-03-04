e2d8662bc58ba305b5b5270a692dc8c231b3a8e3
IvanenkoEgor <bsuir_ivanenko@mail.ru>
2017-03-04 17:22:39 +0300

    Update 3.2.12.cpp

--- 3.2.12.cpp
+++ 3.2.12.cpp
@@ -4,21 +4,29 @@
 using namespace std;
 int main()
 {	
-	setlocale(LC_ALL,"rus");
-	cout<<"Латинский квадрат размером nxn: "<<endl;
-    int **mas, n, i, j;
-    cout<<"Введите n=";
-    cin>>n;
-    mas=new int*[n];
+setlocale(LC_ALL,"rus");
+cout<<"Латинский квадрат размером nxn: "<<endl;
+int **mas, n, i, j;
+cout<<"Введите n=";
+cin>>n;
+mas=new int*[n];
      for(i=0; i<n; i++)
+     {
        mas[i]=new int[n];
-    for(i=0; i<n; i++)
-        for(j=0; j<n; j++)
-            mas[i][j]=(i+j)%n+1;
+     }
     for(i=0; i<n; i++)
     {
         for(j=0; j<n; j++)
+	{
+            mas[i][j]=(i+j)%n+1;
+	}
+    }
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+	{
             cout<<mas[i][j]<<" ";
+	}
         cout<<endl;
     }  
   	getch();
