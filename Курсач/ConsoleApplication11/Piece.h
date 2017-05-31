30f536565ce1ab72024670fae80832da36b70fde
IvanenkoEgor <bsuir_ivanenko@mail.ru>
2017-05-31 22:28:28 +0300

    Add files via upload

---
+++ Курсач/ConsoleApplication11/Piece.h
@@ -0,0 +1,10 @@
+//Declaration
+#pragma once
+class Piece
+{
+public:
+	int x, y, pos_x, pos_y, size_x, size_y, num;
+	Piece(int x, int y, int pos_x, int pos_y, int num);
+	Piece();
+	void GetSize(int field_x, int field_y);
+};
---
+++ Курсач/ConsoleApplication11/main.cpp
@@ -0,0 +1,29 @@
+//Main
+
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+#include "Piece.h"
+#include "Game.h"
+#include "Drawer.h"
+#include "Random.h"
+#include "menu.h"
+
+
+int main()
+{
+	FreeConsole();
+	sf::RenderWindow window(sf::VideoMode(1376, 768), "15-Puzzle");
+	menu(window);
+	Game g;
+	Drawer d;
+	Random r;
+	g.GetEmptyPiece();
+	for (int i = 0; i < 1000; ++i)
+	{
+		r.Randomize(g.my_map, g);
+	}
+	d.DrawField(g.my_map, g);
+	return 0;
+}
